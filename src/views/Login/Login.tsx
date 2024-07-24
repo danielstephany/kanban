9126c917bffab558d5fd29502132d96433a8cebc
Daniel Stephany <danielstephany85@gmail.com>
2024-07-23 23:28:28 -0700

    stubbed out login and signup

---
+++ src/views/Login/Login.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login: React.ElementType = () => {
+    return (
+        <div>login</div>
+    )
+}
+
+export default Login
---
+++ src/views/SignUp/SignUp.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const SignUP: React.ElementType = () => {
+
+    return (
+        <div>sign up</div>
+    )
+}

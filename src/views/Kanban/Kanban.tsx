9b3a73ab1657de918948a57ec5a62f27e0d687d9
Daniel Stephany <danielstephany85@gmail.com>
2024-09-02 15:03:10 -0700

    clean up

--- src/views/Kanban/Kanban.tsx
+++ src/views/Kanban/Kanban.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { Helmet } from "react-helmet"
-import styled from 'styled-components'
-import KanbanDashboard from '@src/components/layouts/KanbanDashboard.tsx'
 import Board from './Board/index.tsx'
 
 const Kanban = () => {

387ba298e9052404253e0932b57b5fdb795bce40
Daniel Stephany <danielstephany85@gmail.com>
2025-06-09 22:11:44 -0700

    updated page settings by adding formctrl

--- src/views/ProjectSettings/ProjectSettings.tsx
+++ src/views/ProjectSettings/ProjectSettings.tsx
@@ -7,6 +7,7 @@ import {
     Button
 } from "@mui/material"
 import LoadingWrapper from '@src/components/modules/LoadingWrapper.tsx'
+import TextFieldFormCtrl from '@src/components/controls/TextFieldFormCtrl'
 import SectionActions from '@src/components/modules/SectionActions'
 import { useParams } from 'react-router-dom'
 import { useSnackbar } from 'notistack'
@@ -16,6 +17,8 @@ import type { boardDataInterface } from '@src/endpoints/board/types.ts'
 import useQuery from '@src/hooks/useQuery.tsx'
 import DeleteBoardModal from '@src/containers/DeleteBoardModal'
 import { errorMessage } from '@src/constants'
+import useFormCtrl from '@src/hooks/useFormCtrl'
+import { Description } from '@mui/icons-material'
 
 export default function ProjectSettings(){
     const {enqueueSnackbar} = useSnackbar()
@@ -23,9 +26,20 @@ export default function ProjectSettings(){
     const { loading, call:callGetBoard, result:boardData } = useQuery<boardDataInterface, string>({fetchFunc: getBoard})
     const [deleteBoardModalOpen, setDeleteBoardModalOpen] = useState(false)
 
+    const formCtrl = useFormCtrl({
+        initialValues: {
+            title: ""
+        }
+    })
+
     const loadBoardData = (id?: string) => {
         if (id) {
             callGetBoard(id)
+            .then(json => {
+                if(json?.title){
+                    formCtrl.setValues({title: json.title})
+                }
+            })
             .catch(e => {
                 enqueueSnackbar(errorMessage, {variant: "error"})
             })
@@ -55,30 +69,37 @@ export default function ProjectSettings(){
                             <Box mt={4}>
                                 <Paper elevation={3}>
                                     <Box p={3}>
-                                        <Grid container spacing={2}>
-                                            <Grid size={{ sm: 12, lg: 6 }}>
-                                                <Typography variant='h3' component="h2">{boardData.title}</Typography>
+                                        <Grid container spacing={4}>
+                                            <Grid size={12}>
+                                                <TextFieldFormCtrl 
+                                                    label="Title"
+                                                    name="title"
+                                                    formCtrl={formCtrl}
+                                                />                                                
+                                            </Grid>                                         
+                                            <Grid size={12}>
+                                                <Typography variant='h4' component="h3" gutterBottom>Column order</Typography>                                            
+                                                <Button variant="outlined" size="small">Reorder columns</Button>
                                             </Grid>
                                             <Grid size={12}>
-                                                
+                                                <Typography variant='h4' component="h3" gutterBottom>Board access</Typography>                                            
+                                                <Button variant="outlined" size="small">Manage users</Button>
                                             </Grid>
-                                        </Grid>
-                                        <Box m={-3} mt={0}>
-                                            <SectionActions
-                                                leftActions={
-                                                    <Button
-                                                        onClick={handleOpenDeleteBoardModal}
-                                                    >Delete Board</Button>
-                                                }
-                                                rightActions={
-                                                    <Button
-                                                        variant='contained'
-                                                        
-                                                    >Edit Board</Button>
-                                                }
-                                            />
-                                        </Box>
+                                        </Grid>                                      
                                     </Box>
+                                    <SectionActions
+                                        leftActions={
+                                            <Button
+                                                onClick={handleOpenDeleteBoardModal}
+                                            >Delete Board</Button>
+                                        }
+                                        rightActions={
+                                            <Button
+                                                variant='contained'
+
+                                            >Edit Board</Button>
+                                        }
+                                    />
                                 </Paper>
                             </Box>
                         : null
